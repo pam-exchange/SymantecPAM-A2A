5d6bb7fdb0ab02c8655d38ae5b099819f89323fe
pam-exchange <admin@pam-exchange.ch>
2025-09-25 13:26:07 +0200

    Updated Python

--- Database-A2A/mssql-CacheCheck.py
+++ Database-A2A/mssql-CacheCheck.py
@@ -1,3 +1,43 @@
+#-----------------------------------------------------------------------
+# Sample program using a Microsoft MSSQL database
+#
+# Optimal approach is to setup A2A account using the A2A client caching 
+# mechanism. With this the A2A client will only query PAM when needed.
+# Alas, most of the time the credentials are found in A2A cache
+# and they are returned very quickly (~ 200mS) instead of a roundtrip to 
+# PAM (~3 seconds}. Drawback is that the application must take into 
+# account that the credentials returned from cache may be obsolete.
+#
+# The example here is handling a scenario where the password retrieved
+# from A2A client is invalid and make a second query but forcing
+# the request to PAM instead of using cache.
+#
+#-----------------------------------------------------------------------
+
+#*********************************************************************
+# MIT License
+#
+# Copyright (c) 2024-2025 PAM-Exchange
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#**********************************************************************
+
 import os
 import platform
 import subprocess
